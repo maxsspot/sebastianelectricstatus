ebd55dce820b603d30515212bb740779e2f75b07
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2021-12-16 20:56:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const captcha = document.getElementById('nobot');
 captcha.addEventListener("click", () => {
 	document.getElementById ("submit").disabled = false;
-  document.getElementById ("submit").style.opacity = "0.0";
-  document.getElementById ("submit").style.cursor = "pointer";
+  document.getElementById ("nobot").style.opacity = "0.0";
+  document.getElementById ("nobot").style.cursor = "pointer";
 });
